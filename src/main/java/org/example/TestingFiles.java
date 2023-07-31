7e28e8b918cf1e87a5c7c1e67435c12b98197041
Weiller Carvalho <engweiller@hotmail.com>
2023-07-31 12:32:17 -0300

    feat: resources add

--- src/main/java/org/example/TestingFiles.java
+++ src/main/java/org/example/TestingFiles.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,12 +10,21 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestingFiles {
     @Test
-    public void test() {
+    public void testTextField() {
 
         WebDriver driver = new ChromeDriver();
         driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/components.html");
-//        driver.manage().window().setSize(new Dimension(1200,765));
-//        Assert.assertEquals("Google", driver.getTitle());
+        driver.findElement(By.name("elementosForm:nome")).sendKeys("Testing");
+        Assert.assertEquals("Testing",driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
         driver.quit();
     }
+    @Test
+    public void testTextArea() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/components.html");
+        driver.findElement(By.name("elementosForm:sugestoes")).sendKeys("Testing");
+        Assert.assertEquals("Testing", driver.findElement(By.name("elementosForm:sugestoes")).getAttribute("value"));
+        driver.quit();
+    }
+
 }
