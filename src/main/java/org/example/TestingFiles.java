6347c3e45fa11036fc20191fd35f0be51f6ee4ad
Weiller Carvalho <engweiller@hotmail.com>
2023-07-31 17:16:23 -0300

    feat: Added more tests.

--- src/main/java/org/example/TestingFiles.java
+++ src/main/java/org/example/TestingFiles.java
@@ -3,9 +3,12 @@ package org.example;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 
 public class TestingFiles {
@@ -44,6 +47,34 @@ public class TestingFiles {
             driver.findElement(By.id("elementosForm:comidaFavorita:" + i)).click();
             Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:" + i)).isSelected());
         }
+        driver.quit();
+    }
+    @Test
+    public void testComboBox() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/components.html");
+        WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
+        Select comboBox = new Select(element);
+        comboBox.selectByIndex(7);
+        Assert.assertEquals("Doutorado", comboBox.getFirstSelectedOption().getText());
+        driver.quit();
+    }
+    @Test
+    public void testListComboBox() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/components.html");
+        WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
+        Select comboBox = new Select(element);
+        List<WebElement> options = comboBox.getOptions();
+        boolean val = false;
+        for (WebElement option: options) {
+            if (option.getText().equals("Superior")) {
+                val = true;
+                break;
+            }
+        }
+        Assert.assertTrue(val);
+        Assert.assertEquals(8, options.size());
         driver.quit();
     }
 }
